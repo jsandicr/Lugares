fdb3474e6f56d880a52a11e15d2ee917f54088e5
jsand <jsandicr16@gmail.com>
2022-10-08 20:08:58 -0600

    Se agrego accion a los botones de registrar e iniciar

--- app/src/main/java/com/jsandi/lugares/MainActivity.kt
+++ app/src/main/java/com/jsandi/lugares/MainActivity.kt
@@ -2,10 +2,65 @@ package com.jsandi.lugares
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import com.jsandi.lugares.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.time.LocalDate
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+
+        binding.btRegister.setOnClickListener(){
+            hacerRegistro();
+        }
+    }
+
+    private fun hacerRegistro(){
+        var email = binding.etMail.text.toString()
+        var clave = binding.etClave.text.toString()
+
+        auth.createUserWithEmailAndPassword(email,clave)
+            .addOnCompleteListener(this){ task ->
+                if(task.isSuccessful){
+                    Log.d("Creando usuario", "Registrado")
+                    val user = auth.currentUser
+                    if(user != null){
+                        actualiza(user)
+                    }
+                }else{
+                    Log.d("Creando Usuario","Fallo")
+                    Toast.makeText(baseContext, "Fallo", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    private fun actualiza(user: FirebaseUser?) {
+        if (user != null){
+            val intent = Intent(this, Principal::class.java)
+            startActivity(intent)
+        }
+    }
+
+    //Autenticado no pida mas inicio de sesion a menos que la cerremos
+    public override fun onStart(){
+        super.onStart()
+        val usuario = auth.currentUser
+        actualiza(usuario)
     }
 }
